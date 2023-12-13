987a9b57b23c9d301d7422b8caee4ce76e870f02
Neharika K <112835898+Neharika5@users.noreply.github.com>
2023-12-13 22:22:07 +0530

    Add files via upload

---
+++ exercise 1/src/Main.java
@@ -0,0 +1,9 @@
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Cat cat = new Cat();
+        animal.makeSound();
+        cat.makeSound();
+    }
+}
